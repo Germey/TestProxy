4374c0f7d0972c7e2ab573cee1ff82e4e8662ecb
Germey <cqc@cuiqingcai.com>
2018-02-03 02:53:38 +0800

    add taiyang

--- test_taiyang.py
+++ test_taiyang.py
@@ -1,12 +1,12 @@
 from public import get_page, test_proxy, max, stats_result, is_proxy
 import time
 
-api_url = 'http://webapi.http.zhimacangku.com/getip?num=1&type=1&pro=&city=0&yys=0&port=1&time=1&ts=0&ys=0&cs=0&lb=4&sb=0&pb=4&mr=1&regions='
+api_url = 'http://http-api.taiyangruanjian.com/getip?num=1&type=1&pro=&city=0&yys=0&port=1&pack=3302&ts=0&ys=0&cs=0&lb=4&sb=0&pb=4&mr=1&regions='
 wait = 6
 
 
 def main():
-    print('Testing zhima')
+    print('Testing taiyang')
     used_time_list = []
     valid_count = 0
     total_count = 0
